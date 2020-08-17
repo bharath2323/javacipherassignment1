a7eedba188ef8ac3ce218fd6f56d7bc7b94c2eda
bharath2323 <49247380+bharath2323@users.noreply.github.com>
2020-08-17 18:59:50 +0530

    Add files via upload

---
+++ Complex.java
@@ -0,0 +1,53 @@
+//Q.5: 
+import java.util.Scanner;
+
+public class Complex {
+    double real;
+    double imag;
+
+    Complex(){
+    	
+    }
+    public Complex(double real, double imag) {
+        this.real = real;
+        this.imag = imag;
+    }
+    public static Complex sum(Complex n1, Complex n2)
+    {
+        Complex temp = new Complex();
+        temp.real = n1.real + n2.real;
+        temp.imag = n1.imag + n2.imag;
+
+        return(temp);
+    }
+    public static Complex difference(Complex n1, Complex n2)
+    {
+        Complex temp = new Complex();
+        temp.real = n1.real - n2.real;
+        temp.imag = n1.imag - n2.imag;
+
+        return(temp);
+    }
+    public static Complex product(Complex n1, Complex n2)
+    {
+        Complex temp = new Complex();
+        temp.real = n1.real * n2.real;
+        temp.imag = n1.imag * n2.imag;
+
+        return(temp);
+    }
+    public static void main(String[] args) {
+    	@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+        Complex n1 = new Complex(sc.nextDouble(), sc.nextDouble());
+        Complex n2 = new Complex(sc.nextDouble(), sc.nextDouble());
+        Complex sum,difference,product;
+
+        sum = sum(n1,n2);
+        difference = difference(n1,n2);
+        product = product(n1,n2);
+        System.out.printf("Sum = %.1f + %.1fi\n", sum.real, sum.imag);
+        System.out.printf("Difference = %.1f + %.1fi\n", difference.real, difference.imag);
+        System.out.printf("Product = %.1f + %.1fi\n", product.real, product.imag);
+    }
+}
